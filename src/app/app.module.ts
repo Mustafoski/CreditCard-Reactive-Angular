1a4c3966477d33fcb68f841cc7fd94b31a7be877
Emir Mustafoski <emirmustafoski92@gmail.com>
2021-12-07 17:08:20 +0100

    add

---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { CardFormComponent } from './card-form/card-form.component';
+import { InputComponent } from './input/input.component';
+import { CardComponent } from './card/card.component';
+
+@NgModule({
+  declarations: [AppComponent, CardFormComponent, InputComponent, CardComponent],
+  imports: [BrowserModule, ReactiveFormsModule, FormsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
